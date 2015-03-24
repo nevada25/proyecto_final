53136aae66435e9beab0e6c232b4fec0e5503ba0
becerra <becerra@192.168.1.37>
2015-03-23 21:31:24 -0500

    la gente empiesa a la venta paa

---
+++ proyecto_final/src/practicando/auto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicando;
+
+
+public class auto {
+    
+    //ATRIBUTOS
+    
+    double peso;
+    double altura;
+    double ancho;
+    double largo ;
+    int numeroPuertas;
+    boolean encendido=false;
+    String modelo;
+
+    public auto(double peso, double altura, double ancho, double largo, int numeroPuertas, String modelo) {
+        this.peso = peso;
+        this.altura = altura;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.numeroPuertas = numeroPuertas;
+        this.modelo = modelo;
+    }
+    
+    
+}
